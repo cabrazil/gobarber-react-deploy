08a12bae96dcf49a211a8849977bcc0e9ba51c78
Carlos Brazil <cabrazil@gmail.com>
2020-12-16 17:22:06 -0300

    gobarber-web tests finalized

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -88,9 +88,10 @@ const AuthProvider: React.FC = ({ children }) => {
 function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
+  // Retirado quando dos testes hooks
+  // if (!context) {
+  //   throw new Error('useAuth must be used within an AuthProvider');
+  // }
   return context;
 }
 
